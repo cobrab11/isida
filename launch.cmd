5a82e94ea78d51ecf847f198389e24d51ee82c94
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-04-26 06:58:35 +0000

    fix: updater

--- launch.cmd
+++
@@ -1,8 +0,0 @@
-# update to actual version
-svn up
-
-#copy version to file
-echo `svnversion` > settings\version
-
-#launch bot
-python start.py
--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 svn up
 
 #copy version to file
-echo `svnversion` > settings\version
+echo `svnversion` > settings/version
 
 #launch bot
 python start.py
