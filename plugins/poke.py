629107f992433d3856911e256e765cd214836cb2
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-07-12 14:17:13 +0000

    add: poke command

---
+++ plugins/poke.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf -*-
+
+def to_poke(type, jid, nick, text):
+	predef_poke = [u'дала NICK... просто дала... :-"',
+			u'потыкала палочкой NICK...',
+			u'предложила NICK козявку :-[',
+			u'накормила NICK пургеном с толчёным стеклом!',
+			u'прошептала NICK в ухо тихонько БУГАГА!',
+			u'целится плюсомётом в NICK и добро улыбается...',
+			u'кинула ломик в сторону NICK',
+			u'дала NICK клубничного йаду',
+			u'попрыгала с бубном вокруг NICK',
+			u'тыкает NICK со словами "купи мороженного, гадка!"']
+	poke_file = 'plugins/poke.txt'
+        ta = get_access(jid,nick)
+        access_mode = ta[0]
+	dpoke = getFile(poke_file,predef_poke)
+	if text == 'show' and access_mode == 2:
+		if type == 'groupchat':
+			send_msg(type, jid, nick, u'Ушло в приват!')
+			type = 'chat'
+		msg = u'Фразы:'
+		cnt = 1
+		for tmp in dpoke:
+			msg += '\n'+str(cnt)+'. '+tmp
+			cnt += 1
+	elif text[:4] == 'del ' and access_mode == 2:
+		text = text[4:]
+		try:
+			pos = int(text)-1
+		except:
+			pos = len(dpoke)+1
+		if pos < 0 or pos > len(dpoke):
+			msg = u'Такой записи нет!'
+		else:
+			remove_body = dpoke[pos]
+			dpoke.remove(remove_body)
+			writefile(poke_file, str(dpoke))
+			msg = u'Удалила: '+remove_body
+
+	elif text[:4] == 'add ' and access_mode == 2:
+		text = text[4:]
+		if text.count('NICK'):
+			dpoke.append(text)
+			writefile(poke_file, str(dpoke))
+			msg = u'Добавила.'
+		else:
+			msg = u'Не могу добавить! Нет ключевого слова "NICK"!'
+	elif text == '' or text == nick:
+		msg = u'Самотык? 8-D'
+	else:
+		mdb = sqlite3.connect(mainbase)
+		cu = mdb.cursor()
+		is_here = cu.execute('select status from age where room=? and nick=?',(jid,text)).fetchone()
+		mdb.close()
+		if is_here == None:
+			msg = u'Я никогда не видела тут '+text
+		elif is_here == (1,):
+			msg = u'Или я дура, или '+text+u' вышел из конфы'
+		else: 
+			msg = '/me '+dpoke[randint(0,len(dpoke)-1)]
+			msg = msg.replace('NICK',text)
+			nick = ''
+	send_msg(type, jid, nick, msg)
+
+global execute
+
+execute = [(0, u'poke', to_poke, 2)]
