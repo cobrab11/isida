914f26919c0fa28e92aed755ac30cc5b72f39f2a
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-07-12 16:43:00 +0000

    add: true command

---
+++ plugins/istrue.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf -*-
+
+def is_true(type, jid, nick, text):
+	if text == '':
+		msg = u'Ну и?'
+	else:
+		idx = 0
+		for tmp in text:
+			idx += ord(tmp)
+		idx = int((idx/100.0 - int(idx/100))*100)
+		msg = u'Вероятность выражения - '+str(idx)+u'%'
+	send_msg(type, jid, nick, msg)
+
+global execute
+
+execute = [(0, u'true', is_true, 2)]
