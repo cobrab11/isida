c08938528643bcb20419c516c1df5c1160c0419c
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-07-19 12:37:01 +0000

    add: turn command

--- plugins/istrue.py
+++ plugins/istrue.py
@@ -9,7 +9,7 @@ def is_true(type, jid, nick, text):
 		for tmp in text:
 			idx += ord(tmp)
 		idx = int((idx/100.0 - int(idx/100))*100)
-		msg = u'Вероятность выражения - '+str(idx)+u'%'
+		msg = u'Вероятность того, что выражение верно - '+str(idx)+u'%'
 	send_msg(type, jid, nick, msg)
 
 global execute
