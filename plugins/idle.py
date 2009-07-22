9a7b54b211670e4d8becc1bb75abe7d1ece93412
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-07-22 12:46:22 +0000

    version 1.8 is out! - change plugins system - split main database to different bases - optimization in seen/age commands - rewrite help command. now helps get from plugins - add: idle plugin

---
+++ plugins/idle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf -*-
+
+idle_base = []
+
+def idle(type, jid, nick, text):
+	global idle_base
+	if not len(text): text = nick
+	msg = u'Не могу найти '+text
+	for tmp in idle_base:
+		if tmp[0] == jid and tmp[1] == text:
+			if text == nick: msg = u'Последняя твоя активность'
+			else: msg = u'Последняя активность '+text
+			msg += u' была '+un_unix(int(time.time())-tmp[3])+u' назад ('
+			if tmp[2] == 'm': msg += u'сообщение)'
+			else: msg += u'презенс)'
+			break
+	send_msg(type, jid, nick, msg)
+
+def append_to_idle(room,jid,nick,type,text):
+	global idle_base
+	for tmp in idle_base:
+		if tmp[0] == room and tmp[1] == nick:
+			idle_base.remove(tmp)
+			break
+	idle_base.append((room,nick,'m',int(time.time())))
+	sys.exit(0)
+
+def remove_from_idle(room,jid,nick,type,text):
+	global idle_base
+	for tmp in idle_base:
+		if tmp[0] == room and tmp[1] == nick:
+			idle_base.remove(tmp)
+			break
+	if type!='unavailable':
+		idle_base.append((room,nick,'p',int(time.time())))
+	sys.exit(0)
+
+global execute
+
+message_control = [append_to_idle]
+presence_control = [remove_from_idle]
+
+execute = [(0, u'idle', idle, 2, u'Время с момента последней активности')]
