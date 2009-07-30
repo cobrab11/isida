60ebb0b9287745e2ce2e2810eafc558557fa7d07
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-07-30 13:50:36 +0000

    add: price plugin by ferym

---
+++ plugins/price.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf -*-
+
+# by ferym@jabbim.org.ru
+
+def price(type, jid, nick, parameters):
+	try:
+		if parameters.count('http://'):
+			send_msg(type, jid, nick, u'формат ввода сайта domain.tld')
+			return
+		if len(parameters):
+			parameters = parameters.split('.')[-2].lower()+'.'+parameters.split('.')[-1].lower()
+			req = 'http://www.webvaluer.org/ru/www.'+parameters
+			r = urllib2.urlopen(req)
+			target = r.read()
+			od = re.search('<span style=\"color:green; font-weight:bold;\">',target)
+			message = target[od.end():]
+			message = message[:re.search('</span></h1>',message).start()]
+			message = message.replace(',','')
+			message = unicode(message.strip(),'utf-8')
+			try: pos = message.find(re.findall(r'[0-9]',message)[0])
+			except: pos = None
+			if pos:
+				if pos >= 2: message = message[pos:]+' '+message[:pos]
+			send_msg(type, jid, nick, u'Оценочная стоимость домена '+parameters.strip()+u' составляет - '+message)
+		else:
+			send_msg(type, jid, nick, u'какой сайт оценивать?')
+	except:
+		send_msg(type, jid, nick, u'Не получилось обработать запрос')
+    
+    
+global execute
+
+
+execute = [(0, u'price', price, 2, u'Показывает ориентировочную оценочную стоимость домена | Author: ferym')]
