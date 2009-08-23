64f3679f80f0e3d0a82bbebff5d43e8702bb51dd
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-08-23 22:27:20 +0000

    add: md5 plugin

---
+++ plugins/md5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf -*-
+
+def md5body(type, jid, nick, text):
+	if len(text): msg = hashlib.md5(text.encode('utf-8')).hexdigest()
+	else: msg = u'Ась?'
+	send_msg(type, jid, nick, msg)
+
+global execute
+
+execute = [(0, u'md5', md5body, 2, u'Расчёт md5 фразы.')]
