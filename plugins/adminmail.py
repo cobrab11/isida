be0d886d3ddf005f4c1575603ff081e9cfee6b40
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-10-03 15:26:36 +0000

    add: adminmail.py plugin by dr.Schmurge (send message for bot owner)

---
+++ plugins/adminmail.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# written by dr.Schmurge
+# fixed by Disabler
+
+time_limit_base = set_folder+u'saytoowner.db'
+amsg_limit = [86400,3600,60] # лимит размера сообщения
+amsg_time_limit = 30 # таймаут отправки в секундах
+
+def adminmail(type, jid, nick, text):
+	if len(text):
+		if len(text) > amsg_limit: text = text[:amsg_limit]+'[...]'
+       		timesent = getFile(time_limit_base, {})
+       		ga = get_access(jid, nick)
+		fjid = getRoom(ga[1])
+		tmp_lim = amsg_limit[ga[0]]
+		if timesent.has_key(fjid):
+                        wt = int(timesent[fjid]-time.time())
+                        if wt >= 0:
+                                send_msg(type, jid, nick, u'Превышен лимит скорости посылки сообщений. Подождите: '+un_unix(wt))
+                                return None
+                        else: del timesent[fjid]
+		timesent[fjid] = int(time.time())+tmp_lim
+		writefile(time_limit_base, str(timesent))
+		msg = u'Пользователь '+nick+u' ['+fjid+u'] из комнаты '+jid+u' в '+str(time.strftime("%H:%M", time.localtime (time.time())))+u' отправил вам сообщение: '+text
+		for ajid in ownerbase: send_msg('chat', getRoom(ajid), '', msg)
+		send_msg(type, jid, nick, u'Отправила')
+	else: send_msg(type, jid, nick, u'Что-то отправить хочешь?')
+
+global execute
+
+execute = [(0, u'msgtoadmin', adminmail, 2, u'Отправить сообщение владельцу бота.\nmsgtoadmin текст сообщения')]
