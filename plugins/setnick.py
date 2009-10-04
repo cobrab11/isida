2d5ee5188af876419432ffe2c1391cd06a6a180f
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-10-04 11:14:15 +0000

    fix: setnick.py plugin

--- plugins/setnick.py
+++ plugins/setnick.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 def set_nickname(type, jid, nick, text):
-	if get_affiliation(jid,nick) != 'owner': msg = u'Тибе низя!'
-	else:
+	if get_affiliation(jid,nick) == 'owner' or get_access(jid,nick)[0] == 2:
 		msg = None
 		if text == '': text = jid+'/'+nickname
 		else: text = jid+'/'+text
+	else: msg = u'Тибе низя!'
 	if msg: send_msg(type, jid, nick, msg)
 	else: bot_join(type, jid, nick, text)
 
