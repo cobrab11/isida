3a5717cc9d2109f7aeb82e6299881b9c84171810
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-10-10 18:43:16 +0000

    fix: svn command

--- plugins/svn.py
+++ plugins/svn.py
@@ -2,23 +2,25 @@
 # -*- coding: utf -*-
 
 def svn_get(type, jid, nick,text):
-	if text[:7] !='http://' and text[:8] !='https://' and text[:6] !='svn://': text = 'http://'+text
-	count = 1
-	revn = 0
-	if text.count(' '):
-		text = text.split(' ')
-		url = text[0]
-		try: count = int(text[1])
-		except:
-			try:
-				if text[1].lower().count('r'): revn = int(text[1][text[1].find('r')+1:])
-			except: revn = 0
-	else: url=text
-	if revn != 0: sh_exe = 'bash -c \"LANG=ru_RU.UTF8 svn log '+url+' -r'+str(revn)+'\"'
-	else:
-		if count > 10: count = 10
-		sh_exe = 'bash -c \"LANG=ru_RU.UTF8 svn log '+url+' --limit '+str(count)+'\"'
-	msg = u'SVN from '+url+'\n'+shell_execute(sh_exe)
+	if len(text):
+		if text[:7] !='http://' and text[:8] !='https://' and text[:6] !='svn://': text = 'http://'+text
+		count = 1
+		revn = 0
+		if text.count(' '):
+			text = text.split(' ')
+			url = text[0]
+			try: count = int(text[1])
+			except:
+				try:
+					if text[1].lower().count('r'): revn = int(text[1][text[1].find('r')+1:])
+				except: revn = 0
+		else: url=text
+		if revn != 0: sh_exe = 'bash -c \"LANG=ru_RU.UTF8 svn log '+url+' -r'+str(revn)+'\"'
+		else:
+			if count > 10: count = 10
+			sh_exe = 'bash -c \"LANG=ru_RU.UTF8 svn log '+url+' --limit '+str(count)+'\"'
+		msg = u'SVN from '+url+'\n'+shell_execute(sh_exe)
+	else: msg = u'Изучаем инструкции к командам...'
 	send_msg(type, jid, nick, msg)
 	
 global execute
