d36c52b0306ce9f8af585379a361a642b9b8d0cd
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-10-16 13:04:25 +0000

    fix: sayto command (need more fixes)

--- plugins/sayto.py
+++ plugins/sayto.py
@@ -3,28 +3,40 @@
 
 def sayto(type, jid, nick, text):
 	if text.count(' '):
-		to = text[:text.find(' ')]
-		what = text[text.find(' ')+1:]
+		to = text.split(' ')[0]
+		what = text.split(' ',1)[1]
 		frm = nick + '\n' + str(int(time.time()))
 		mdb = sqlite3.connect(agestatbase)
 		cu = mdb.cursor()
-		fnd = cu.execute('select * from age where room=? and (nick=? or jid=?)',(jid,to,to)).fetchall()
+		fnd = cu.execute('select status, jid from age where room=? and nick=? group by jid ',(jid,to)).fetchall()
 		if len(fnd) == 1:
 			fnd = fnd[0]
-			if fnd[5]:
+			if fnd[0]:
 				msg = u'Передам'
 				sdb = sqlite3.connect(saytobase)
 				cu = sdb.cursor()
-				cu.execute('insert into st values (?,?,?,?)', (frm, jid, fnd[2], what))
+				cu.execute('insert into st values (?,?,?,?)', (frm, jid, fnd[1], what))
 				sdb.commit()
 			else: msg = u'Или я дура, или '+to+u' находится тут...'
-		elif len(fnd) > 1: msg = u'Я видела несколько человек с таким ником. Укажите точнее!'
-		else:
-			msg = u'Я не в курсе кто такой '+to+u'. Могу не правильно передать.'
+		elif len(fnd) > 1:
+			off_count = 0
 			sdb = sqlite3.connect(saytobase)
 			cu = sdb.cursor()
-			cu.execute('insert into st values (?,?,?,?)', (frm, jid, to, what))
+			for tmp in fnd:
+				if tmp[0]:
+					cu.execute('insert into st values (?,?,?,?)', (frm, jid, tmp[1], what))
+					off_count += 1
 			sdb.commit()
+			if off_count: msg = u'Я видела несколько человек с таким ником и могу ошибочно передать. Совпадений: '+str(len(fnd))+u', будет передано: '+str(off_count)
+			else: msg = u'Все, кого я видела с таким ником, находятся в конфе!'
+		else:
+			if to.count('@') and to.count('.'):
+				msg = u'Я не видела человека с jid\'ом '+to+u', но если зайдёт - я передам.'
+				sdb = sqlite3.connect(saytobase)
+				cu = sdb.cursor()
+				cu.execute('insert into st values (?,?,?,?)', (frm, jid, to, what))
+				sdb.commit()
+			else: msg = u'Необходимо указать jid!'
 	else: msg = u'Кому что передать?'
 	send_msg(type, jid, nick, msg)
 
