a5e8eabe8aee302fb6b853902bb4360511b62345
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-10-28 18:33:54 +0000

    add: writing cron command is start again. need more wishes about it

--- plugins/cron.py
+++ plugins/cron.py
@@ -3,8 +3,26 @@
 
 cron_base = set_folder+u'cron.db'	# база заданий
 
+# room type{} repeat time
+# cron add timer 10:00 [repeat 5:00] command
+# cron add alarm 2009-10-29 10:00 [repeat 24:00:00] command
+
 def time_cron(type, jid, nick, text):
+	ar = text.lower().split(' ',1)
+	if ar[0] == 'show': msg = time_cron_show(ar[1])
+	elif ar[0] == 'add': msg = time_cron_add(ar[1])
+	elif ar[0] == 'del': msg = time_cron_del(ar[1])
+	else: msg = u'Ась?'
 	send_msg(type, jid, nick, msg)
+
+def time_cron_show(ar):
+	return u'Не умею!'
+
+def time_cron_add(ar):
+	return u'Не умею!'
+	
+def time_cron_del(ar):
+	return u'Не умею!'
 
 def cron_action():
 	return
