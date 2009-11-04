be77067b8acb4ef90d0b846ab60bed972e54b2aa
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-11-04 14:13:00 +0000

    chg: domain_info command

--- plugins/domain.py
+++ plugins/domain.py
@@ -3,11 +3,12 @@
 
 def domaininfo(type, jid, nick, text):
 	if text.count('sites'): t, text, spl, msg = 2, text.split(' ')[1], '<font color="black" size="2">', u'Сайты расположенные на IP адресе/домене: '+text.split(' ')[1]+u'\n'
-	else: t, spl, msg = 1, '<blockquote>', u'Информация о IP адресе/домене:\n'
+	else: t, spl, msg = 1, '<blockquote>', u'Информация о IP адресе/домене:'
 	url = u'http://1whois.ru/index.php?url='+str(text)+u'&t='+str(t)
 	body = html_encode(urllib.urlopen(url).read().replace('&nbsp;', ' '))
-	body = body.split(spl)[1].split('Generation time')[0]
-	msg += replacer(body)
+	body = body.split(spl)[1].split('Generation time:')[0]
+	if body.count(u'Нет данных!'): msg += u' Нет данных!'
+	else: msg += '\n'+replacer(body)
 	send_msg(type, jid, nick, msg)
 	
 
