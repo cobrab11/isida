ddb276e230989a20848c9bf678692f3bac76426a
johanschmrge <johanschmrge@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-11-13 17:45:51 +0000

    fix: blacklist plugin

--- plugins/blacklist.py
+++ plugins/blacklist.py
@@ -3,37 +3,53 @@
 
 blacklist_base = set_folder + u'blacklist.db'
 
-def check_blacklist(room):
-	lst = getFile(blacklist_base, [])
-	try:
-		lst.index(room)
-		return True
-	except: return False
+def leave_room(rjid, reason):
+	global confbase, confs
+	msg = u''
+	for i in range(0, len(confbase)):
+		jjid = confbase[i]
+		if rjid == jjid[:jjid.find('/')]:
+			confbase.remove(jjid)
+			writefile(confs, str(confbase))
+			leaveconf(rjid, domain, reason)
+			msg = u'Свалила из '+ rjid + '\n'
+	return msg
 
 def blacklist(type, jid, nick, text): 
-	text, msg = unicode(text), u''
+	global confbase, lastserver
+	text, msg = unicode(text.lower()), u''
 	templist = getFile(blacklist_base, [])
-	text = text.split(' ')
-	if text[0] == 'add':
-		if text[1] in templist: msg = u'Конференция уже находится в черном списке.'
-		else:
-			templist.append(text[1])
-			msg = u'Добавлено: '+text[1]
-	elif text[0] == 'del':
-		if text[1] in templist: msg = u'Удалена: '+str(templist.pop(templist.index(text[1])))
-		else: msg = u'Конференция отсутствует в списке.'
-	elif text[0] == 'show':
-		msg, n = u'Список конференций:\n', 1
-		for i in range(0, len(templist)):
-			msg += str(n)+'. '+templist[i]+'\n'
-			n += 1
-		msg = msg[:-1]
-	elif text[0] == 'clear': msg, templist = u'Очищено.', []
-	else: msg = u'Ошибка в параметрах, прочитайте помощь по команде.'
+	reason = u'Конференция находится в черном списке'
+	try:
+		text = text.split(' ')
+		if not text[1].count('@'): text[1] += '@'+lastserver
+		if text[0] == 'add':
+			if text[1] in templist: msg = u'Адрес уже содержится в списке.'
+			elif len(confbase)==1 and confbase[0].split('/')[0] == text[1]:
+				msg =u'Нельзя добавлять последнюю конференцию в черный список'
+			else:
+				msg = leave_room(text[1], reason)
+				templist.append(text[1])
+				msg += u'Добавлено в черный список: '+str(text[1])
+		elif text[0] == 'del':
+			if text[1] in templist: msg = u'Удалено из черного списка: '+str(templist.pop(templist.index(text[1])))
+			else: msg = u'Адрес отсутствует в списке.'
+		else: msg = u'Ошибка в параметрах, прочитайте помощь по команде.'
+	except:
+		if text[0] == 'show':
+			if len(templist) == 0: msg = u'Список пуст'
+			else:
+				msg, n = u'Список конференций:\n', 1
+				for i in range(0, len(templist)):
+					msg += str(n)+'. '+templist[i]+'\n'
+					n += 1
+				msg = msg[:-1]
+		elif text[0] == 'clear': msg, templist = u'Очищено.', []
+		else: msg = u'Ошибка в параметрах, прочитайте помощь по команде.'
 	writefile(blacklist_base, str(templist))
 	send_msg(type, jid, nick, msg)
 
 
 global execute
 
-execute = [(2, u'blacklist', blacklist, 2, u'Черный список конференций.\nblacklist add|del|show|clear')]
+execute = [(2, u'blacklist', blacklist, 2, u'Черный список конференций.\nblacklist add|del|show|clear\nblacklist add|del room@conference.server.tld - добавить|удалить конферецию из черного списка\nblacklist show - показать список\nblacklist clear - очистить список')]
