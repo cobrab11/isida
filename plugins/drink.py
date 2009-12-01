43cfbe90fdaa461bfc3575403a58a660f8b61e87
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-12-01 12:57:51 +0000

    fix: drink plugin

--- plugins/drink.py
+++ plugins/drink.py
@@ -18,38 +18,26 @@ def to_drink(type, jid, nick, text):
 		ddate = readfile(date_file).decode('UTF')
 		week1 = u''
 		week2 = u''
-		if ddate == '':
-			msg = u'Ошибка чтения файла!'
+		if ddate == '': msg = u'Ошибка чтения файла!'
 		else:
 			if len(text) <= 2:
 				ltim = tuple(localtime())
 				text = str(ltim[2])+' '+mmas2[ltim[1]-1]
-
-				if ltim[0]/4.0 == int(ltim[0]/4):
-					mtab = [31,29,31,30,31,30,31,31,30,31,30,31]
-				else:
-					mtab = [31,28,31,30,31,30,31,31,30,31,30,31]
+				if ltim[0]/4.0 == int(ltim[0]/4): mtab = [31,29,31,30,31,30,31,31,30,31,30,31]
+				else: mtab = [31,28,31,30,31,30,31,31,30,31,30,31]
 				week1 = str(int(ltim[2]/7.0)+1*(int(ltim[2]/7.0)!=(ltim[2]/7.0))) + u' '+wday[ltim[6]]+' '+mmas2[ltim[1]-1]
-				if ltim[2]+7 > mtab[ltim[1]]:
-					week2 = lday[ltim[6]]+u' '+wday[ltim[6]]+u' '+mmas2[ltim[1]-1]
-
+				if ltim[2]+7 > mtab[ltim[1]-1]: week2 = lday[ltim[6]]+u' '+wday[ltim[6]]+u' '+mmas2[ltim[1]-1]
 			or_text = text
-			if text.count('.')==1:
-				text = text.split('.')
-			elif text.count(' ')==1:
-				text = text.split(' ')
-			else:
-				text = [text]
+			if text.count('.')==1: text = text.split('.')
+			elif text.count(' ')==1: text = text.split(' ')
+			else: text = [text]
 			msg = ''
 			ddate = ddate.split('\n')
 			ltxt = len(text)
 			for tmp in ddate:
-				if tmp.lower().count(or_text.lower()):
-					msg += '\n'+tmp
-				elif tmp.lower().count(week1.lower()) and week1 != '':
-					msg += '\n'+tmp
-				elif tmp.lower().count(week2.lower()) and week2 != '':
-					msg += '\n'+tmp
+				if tmp.lower().count(or_text.lower()): msg += '\n'+tmp
+				elif tmp.lower().count(week1.lower()) and week1 != '': msg += '\n'+tmp
+				elif tmp.lower().count(week2.lower()) and week2 != '': msg += '\n'+tmp
 				else:
 					try:
 						ttmp = tmp.split(' ')[0].split('.')
@@ -62,15 +50,10 @@ def to_drink(type, jid, nick, text):
 						t = tday.index(text[0])
 						t = tmonth.index(text[1])
 						msg += '\n'+tmp
-					except:
-						t = None
-
-			if msg == '':
-				msg = u'Повод '+or_text+u' не найден!'
-			else:
-				msg = u'Я знаю повод(ы) выпить:'+msg
-	else:
-		msg =u'К сожалению база отсутствует.'
+					except: pass
+			if msg == '': msg = u'Повод '+or_text+u' не найден!'
+			else: msg = u'Я знаю повод(ы) выпить:'+msg
+	else: msg =u'К сожалению база отсутствует.'
 	send_msg(type, jid, nick, msg)
 
 global execute
