fb59ace0203e60242c302859282edb550718905b
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2009-12-26 16:31:29 +0000

    fix: some fixes in wz plugin

--- plugins/wz.py
+++ plugins/wz.py
@@ -3,6 +3,11 @@
 
 wzbase = set_folder+u'wz.db'
 
+def check_wz(text):
+	for tm in text:
+		if ord(tm)<33 or ord(tm)>127: return True
+	return None
+
 def get_weather(text):
 	cbb = sqlite3.connect(wzbase)
 	cu = cbb.cursor()
@@ -18,106 +23,97 @@ def get_weather(text):
 	return wzz
 
 def weather(type, jid, nick, text):
-	for tm in text:
-		if ord(tm)<33 or ord(tm)>127:
-			send_msg(type, jid, nick, u'Ошибка в параметрах!')
-			return
-	wzz = get_weather(text)
-	if wzz.count('Not Found'): msg = u'Город не найден!'
+	if check_wz(text): msg = u'Ошибка в параметрах!'
 	else:
-		wzz = wzz.split('\n')
-		wzr = []
-		wzr.append(wzz[0])			# 0
-		wzr.append(wzz[1])			# 1
-		wzr.append(sfind(wzz,'Temperature'))	# 2
-		wzr.append(sfind(wzz,'Wind'))		# 3
-		wzr.append(sfind(wzz,'Relative'))	# 4
-		wzr.append(sfind(wzz,'Sky'))		# 5
-		wzr.append(sfind(wzz,'Weather'))	# 6
-		wzr.append(sfind(wzz,'Visibility'))	# 7
-		wzr.append(sfind(wzz,'Pressure'))	# 8
-		if wzr[0].count(')'): msg = wzr[0][:wzr[0].find(')')+1]
-		else: msg = wzr[0]
-		msg += '\n'+ wzr[1]
-		wzz1 = wzr[2].find(':')+1 # Temperature
-		wzz2 = wzr[2].find('(',wzz1)
-		wzz3 = wzr[2].find(')',wzz2)
-		msg += '\n'+ wzr[2][:wzz1] + ' ' + wzr[2][wzz2+1:wzz3]
-		wzz1 = wzr[3].find('(')
-		wzz2 = wzr[3].find(')',wzz1)
-		wzz3 = wzr[3].find(':',wzz2)
-		msg += '\n'+ wzr[3][:wzz1-1] + wzr[3][wzz2+1:wzz3]
-		msg += '\n'+ wzr[4]
-		if len(wzr[5]): msg += ','+ wzr[5][wzr[5].find(':')+1:]
-		if len(wzr[6]): msg += ','+ wzr[6][wzr[6].find(':')+1:]
-		if not (len(wzr[5])+len(wzr[6])): msg += ', clear'
-		msg += '\n'+ wzr[7][:-2]
-		wzz1 = wzr[8].find('(')
-		wzz2 = wzr[8].find(':',wzz1)
-		wzz3 = wzr[8].find('(',wzz2)
-		msg += ', '+ wzr[8][:wzz1-1]+': '+wzr[8][wzz3+1:-1]
+		wzz = get_weather(text)
+		if wzz.count('Not Found'): msg = u'Город не найден!'
+		else:
+			wzz = wzz.split('\n')
+			wzr = []
+			wzr.append(wzz[0])			# 0
+			wzr.append(wzz[1])			# 1
+			wzr.append(sfind(wzz,'Temperature'))	# 2
+			wzr.append(sfind(wzz,'Wind'))		# 3
+			wzr.append(sfind(wzz,'Relative'))	# 4
+			wzr.append(sfind(wzz,'Sky'))		# 5
+			wzr.append(sfind(wzz,'Weather'))	# 6
+			wzr.append(sfind(wzz,'Visibility'))	# 7
+			wzr.append(sfind(wzz,'Pressure'))	# 8
+			if wzr[0].count(')'): msg = wzr[0][:wzr[0].find(')')+1]
+			else: msg = wzr[0]
+			msg += '\n'+ wzr[1]
+			wzz1 = wzr[2].find(':')+1 # Temperature
+			wzz2 = wzr[2].find('(',wzz1)
+			wzz3 = wzr[2].find(')',wzz2)
+			msg += '\n'+ wzr[2][:wzz1] + ' ' + wzr[2][wzz2+1:wzz3]
+			wzz1 = wzr[3].find('(')
+			wzz2 = wzr[3].find(')',wzz1)
+			wzz3 = wzr[3].find(':',wzz2)
+			msg += '\n'+ wzr[3][:wzz1-1] + wzr[3][wzz2+1:wzz3]
+			msg += '\n'+ wzr[4]
+			if len(wzr[5]): msg += ','+ wzr[5][wzr[5].find(':')+1:]
+			if len(wzr[6]): msg += ','+ wzr[6][wzr[6].find(':')+1:]
+			if not (len(wzr[5])+len(wzr[6])): msg += ', clear'
+			msg += '\n'+ wzr[7][:-2]
+			wzz1 = wzr[8].find('(')
+			wzz2 = wzr[8].find(':',wzz1)
+			wzz3 = wzr[8].find('(',wzz2)
+			msg += ', '+ wzr[8][:wzz1-1]+': '+wzr[8][wzz3+1:-1]
 	send_msg(type, jid, nick, msg)
 
 def weather_short(type, jid, nick, text):
-	for tm in text:
-		if ord(tm)<33 or ord(tm)>127:
-			send_msg(type, jid, nick, u'Ошибка в параметрах!')
-			return
-	wzz = get_weather(text)
-	if wzz.count('Not Found'): msg = u'Город не найден!'
+	if check_wz(text): msg = u'Ошибка в параметрах!'
 	else:
-		wzz = wzz.split('\n')
-		wzr = []
-		wzr.append(wzz[0])			# 0
-		wzr.append(sfind(wzz,'Temperature'))	# 2
-		wzr.append(sfind(wzz,'Wind'))		# 3
-		wzr.append(sfind(wzz,'Relative'))	# 4
-		wzr.append(sfind(wzz,'Sky'))		# 5
-		wzr.append(sfind(wzz,'Weather'))	# 6
-		if wzr[0].count(')'): msg = wzr[0][:wzr[0].find(')')+1]
-		else: msg = wzr[0]
-		wzz1 = wzr[1].find(':')+1 # Temperature
-		wzz2 = wzr[1].find('(',wzz1)
-		wzz3 = wzr[1].find(')',wzz2)
-		msg += ' | '+ wzr[1][:wzz1] + ' ' + wzr[1][wzz2+1:wzz3]
-		wzz1 = wzr[2].find('(')
-		wzz2 = wzr[2].find(')',wzz1)
-		wzz3 = wzr[2].find(':',wzz2)
-		msg += ' | '+ wzr[2][:wzz1-1] + wzr[2][wzz2+1:wzz3]
-		msg += ' | '+ wzr[3]
-		if len(wzr[4]): msg += ','+ wzr[4][wzr[4].find(':')+1:]
-		if len(wzr[5]): msg += ','+ wzr[5][wzr[5].find(':')+1:]
-		if not (len(wzr[4])+len(wzr[5])): msg += ', clear'
+		wzz = get_weather(text)
+		if wzz.count('Not Found'): msg = u'Город не найден!'
+		else:
+			wzz = wzz.split('\n')
+			wzr = []
+			wzr.append(wzz[0])			# 0
+			wzr.append(sfind(wzz,'Temperature'))	# 2
+			wzr.append(sfind(wzz,'Wind'))		# 3
+			wzr.append(sfind(wzz,'Relative'))	# 4
+			wzr.append(sfind(wzz,'Sky'))		# 5
+			wzr.append(sfind(wzz,'Weather'))	# 6
+			if wzr[0].count(')'): msg = wzr[0][:wzr[0].find(')')+1]
+			else: msg = wzr[0]
+			wzz1 = wzr[1].find(':')+1 # Temperature
+			wzz2 = wzr[1].find('(',wzz1)
+			wzz3 = wzr[1].find(')',wzz2)
+			msg += ' | '+ wzr[1][:wzz1] + ' ' + wzr[1][wzz2+1:wzz3]
+			wzz1 = wzr[2].find('(')
+			wzz2 = wzr[2].find(')',wzz1)
+			wzz3 = wzr[2].find(':',wzz2)
+			msg += ' | '+ wzr[2][:wzz1-1] + wzr[2][wzz2+1:wzz3]
+			msg += ' | '+ wzr[3]
+			if len(wzr[4]): msg += ','+ wzr[4][wzr[4].find(':')+1:]
+			if len(wzr[5]): msg += ','+ wzr[5][wzr[5].find(':')+1:]
+			if not (len(wzr[4])+len(wzr[5])): msg += ', clear'
 	send_msg(type, jid, nick, msg)
 
 def weather_raw(type, jid, nick, text):
-	for tm in text:
-		if ord(tm)<33 or ord(tm)>127:
-			send_msg(type, jid, nick, u'Ошибка в параметрах!')
-			return
-	wzz = get_weather(text)
-	msg = msg[:-1]
-	if msg.count('Not Found'): msg = u'Город не найден!'
+	if check_wz(text): msg = u'Ошибка в параметрах!'
+	else:
+		msg = get_weather(text)[:-1]
+		if msg.count('Not Found'): msg = u'Город не найден!'
 	send_msg(type, jid, nick, msg)
 
 def weather_city(type, jid, nick, text):
-	for tm in text:
-		if ord(tm)<32 or ord(tm)>127:
-			send_msg(type, jid, nick, u'Ошибка в параметрах!')
-			return
-	try: cnt = int(text.split(' ',text.count(' '))[text.count(' ')])
-	except: cnt = 10
-	if cnt < 1: cnt = 1
-	elif cnt > 100: cnt = 100
-	text = text.split(' ',1)[0]
-	cbb = sqlite3.connect(wzbase)
-	cu = cbb.cursor()
-	wzc = cu.execute('select * from wz where code like ? or city like ? or counry like ? order by code',(text,text,text)).fetchall()
-	cbb.close()
-	if not wzc: msg = u'Такой город не найден!'
+	if check_wz(text): msg = u'Ошибка в параметрах!'
 	else:
-		msg = u'Найдено:'
-		for tmp in wzc[:cnt]: msg += '\n'+tmp[0]+' - '+tmp[1]+' ('+tmp[2]+')'
+		try: cnt = int(text.split(' ',text.count(' '))[text.count(' ')])
+		except: cnt = 10
+		if cnt < 1: cnt = 1
+		elif cnt > 100: cnt = 100
+		text = text.split(' ',1)[0]
+		cbb = sqlite3.connect(wzbase)
+		cu = cbb.cursor()
+		wzc = cu.execute('select * from wz where code like ? or city like ? or counry like ? order by code',(text,text,text)).fetchall()
+		cbb.close()
+		if not wzc: msg = u'Такой город не найден!'
+		else:
+			msg = u'Найдено:'
+			for tmp in wzc[:cnt]: msg += '\n'+tmp[0]+' - '+tmp[1]+' ('+tmp[2]+')'
 	send_msg(type, jid, nick, msg)
 
 global execute
