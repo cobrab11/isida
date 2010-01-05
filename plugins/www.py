2d3e5fa2d1aef1289624651cf7aacc140934deff
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2010-01-05 12:56:23 +0000

    fix: regexp in www command (issue 61)

--- plugins/www.py
+++ plugins/www.py
@@ -5,7 +5,7 @@ user_agent='Mozilla/5.0 (X11; U; Linux x86_64; ru; rv:1.9.0.4) Gecko/2008120916 
 
 def netheader(type, jid, nick, text):
 	if len(text):
-		if text[:7] !='http://': text = 'http://'+text
+		if not text.count('://'): text = 'http://'+text
 		req = urllib2.Request(text)
 		req.add_header('User-Agent',user_agent)
 		try: body = str(urllib2.urlopen(req).headers)
@@ -15,32 +15,27 @@ def netheader(type, jid, nick, text):
 
 def netwww(type, jid, nick, text):
 	try:
-		regex = text.split('\n')[0]
+		regex = text.split('\n')[0].replace('*','*?')
 		text = text.split('\n')[1]
 	except: regex = None
-
 	text = text.encode('utf-8').replace('\\x','%').replace(' ','%20')
-	if text[:7] !='http://': text = 'http://'+text
-
+	if not text.count('://'): text = 'http://'+text
 	req = urllib2.Request(text)
 	req.add_header('User-Agent',user_agent)
 	try: page = urllib2.urlopen(req).read()
 	except: page = u'forbidden'
-
 	page = html_encode(page)
-#	page = rss_replace(page)
 	if regex:
-		mt = re.findall(r''+regex, page, re.S)
-#		for a in mt:
-#			print a[:50]
-		if mt != []:
-			mt = tuple(mt[0])
-			msg = ''
-			for tmp in mt: msg += tmp
-		else: msg = u'RegExp не найден!'
-	else:
-		msg = get_tag(page,'title')+'\n'
-		msg += unhtml(page)
+		try:
+			mt = re.findall(regex, page, re.S)
+			if mt != []:
+				mt = tuple(mt[0])
+				msg = ''
+				for tmp in mt: msg += tmp + ' '
+				msg = unhtml(msg)
+			else: msg = u'RegExp не найден!'
+		except: msg = u'Ошибка в RegExp!'
+	else: msg = get_tag(page,'title')+'\n'+unhtml(page)
 	send_msg(type, jid, nick, msg[:msg_limit])
 
 global execute
