b9dfae174aabefb73f26cd1ccca2a9b18f7412d2
veganet.org <veganet.org@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2010-03-01 21:09:47 +0000

    add: demo_config.en.py (The English version)

---
+++ settings/demo_config.en.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*- 
+
+#------------------------------------------------
+#             Isida-bot Config file
+#                  v1.2beta
+#------------------------------------------------
+
+
+# nick a bot.!!! There is no check on employment nick!!! We register jid and nick, and then we fill!
+# jid a bot will be such: name@domain/mainRes
+nickname = u'<write here bot nick>'
+
+# bot login
+name = u'isida-jabber-bot'
+
+# bot server
+domain = u'domain.tld'
+
+# Bot resource in conference
+mainRes = u'isida-bot'
+
+# password
+password = u'********'
+
+# bot owner jid
+SuperAdmin = u'aaa@bbb.ru'
+
+# start conference
+defaultConf = u'isida@conference.jabber.ru'
+
+# bot status chat|online|away|xa|dnd
+CommStatus = u'online'
+
+# status-message
+StatusMessage = u'i am isida bot'
+
+# priority
+Priority = 0
+
+# command prefix
+prefix = u'_'
+
+# Limit of the size of messages
+msg_limit = 1000
+
+# ------- debugging! -------
+
+# Mode _not_to_ignore_an_error_
+#debugmode = True
+
+# Debugging mode xmpppy
+#dm = True
+
+# Mode of show of operations of a bot in console
+#dm2 = True
+
+# logging commands of a bot
+CommandsLog = True
