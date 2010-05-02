f2a020b6490e5b92b0e88d9aa2fe9c747d8dc070
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2010-05-02 10:48:27 +0000

    fix: join conference

--- kernel.py
+++ kernel.py
@@ -343,7 +343,9 @@ def join(conference):
 	answered, Error, join_timeout = None, None, 3
 	while not answered and join_timeout and not game_over:
 		if is_start:
-			if int(cl.Process(1)): cycles_used += 1
+			cyc = cl.Process(1)
+			if str(cyc) == 'None': cycles_unused += 1
+			elif int(str(cyc)): cycles_used += 1
 			else: cycles_unused += 1
 		else:
 			sleep(1)
