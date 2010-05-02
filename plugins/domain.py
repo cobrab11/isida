746f72a560ce409ed3c3e93b2097332cc58e2a50
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2010-05-02 15:48:26 +0000

    fix: domain_info command

--- plugins/domain.py
+++ plugins/domain.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 def domaininfo(type, jid, nick, text):
-	if text.count('sites'): t, text, spl, msg = 2, text.split(' ')[1], '<font color="black" size="2">', L('Sites at Domain/IP address: %s\n') % text.split(' ')[1]
-	else: t, spl, msg = 1, '<blockquote>', L('Domain/IP address info:')
-	url = 'http://1whois.ru/index.php?url='+str(text)+'&t='+str(t)
+	if text.lower().count('sites'): t, text, regex, msg = 2, text.split(' ')[1], '<font color="black" size="2">(.*?)</font>', L('Sites at Domain/IP address: %s\n') % text.split(' ')[1]
+	else: t, regex, msg = 1, '<blockquote>(.*?)</blockquote>', L('Domain/IP address info:')
+	url = 'http://1whois.ru/index.php?url=%s&t=%s' % (text,t)
 	body = html_encode(urllib.urlopen(url).read().replace('&nbsp;', ' '))
-	body = body.split(spl)[1].split('Generation time:')[0]
+	body = re.findall(regex, body, re.S)[0]
 	if body.count(u'Нет данных!'): msg += ' '+ L('No data!')
 	else: msg += '\n'+replacer(body)
 	send_msg(type, jid, nick, msg)
