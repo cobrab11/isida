37ece5d8835b80800e68a0285164046e57e9b757
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2010-06-01 14:17:04 +0000

    fix: censor in turn command

--- plugins/turn.py
+++ plugins/turn.py
@@ -28,8 +28,7 @@ def turner(type, jid, nick, text):
 				notur = 0
 				break
 		if notur: msg += tex
-	gl_censor = getFile(cns,[(getRoom(jid),0)])
-	if int((getRoom(jid),1) in gl_censor): msg = to_censore(msg)
+	if get_config(getRoom(room),'censor'): msg = to_censore(msg)
 	send_msg('groupchat', jid, '', msg)
 
 def append_to_turner(room,jid,nick,type,text):
