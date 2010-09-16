7e5567bf65b4c44b4147c8a66fff96a39e6384b9
poluphanov.vitaliy <poluphanov.vitaliy@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2010-09-16 19:40:43 +0000

    fix: google define

--- plugins/googleplus.py
+++ plugins/googleplus.py
@@ -4,7 +4,7 @@
 # (c) Vit@liy
 
 def gcalc(type, jid, nick, text):
-	if not len(text) or len(text) == text.count(' '): msg = L('What?')
+	if not text.strip(): msg = L('What?')
 	else:
 		query=urllib.urlencode({'q':text.encode('utf-8')})
 		start='<h2 class=r style="font-size:138%"><b>'
@@ -22,6 +22,39 @@ def gcalc(type, jid, nick, text):
 			msg = result.decode('cp1251')
 	send_msg(type, jid, nick, msg)
 
+def define(type, jid, nick, text):
+	text = text.strip()
+	target = ''
+	if not text: msg = L('What?')
+	else:
+		if re.search('\A\d+?(-\d+?)? ', text): target, text = text.split(' ', 1)
+		query=urllib.urlencode({'q':'define:'+text.encode('utf-8')})
+		start='<h2 class=r style="font-size:138%"><b>'
+		end='</b>'
+		google=httplib.HTTPConnection("www.google.ru")
+		google.request("GET","/search?"+query)
+		search=google.getresponse()
+		data=search.read()
+		result = re.findall('<li>(.+?)<font color=#008000>(.+?)</font></a><p>', data)
+
+		if not result: msg = L('Define not found')
+		else:
+			if target:
+				try: n1 = n2 = int(target)
+				except: n1, n2 = map(int, target.split('-'))
+				msg = ''
+				if 0 < n1 <= n2 <= len(result): 
+					for k in xrange(n1-1,n2): msg += result[k][0] + '\nhttp://' + result[k][1] + '\n\n'
+				else: msg = L('Define not found')
+			else:
+				result = random.choice(result)
+				msg = result[0] + '\nhttp://' + result[1]
+			msg = re.sub(r'<[^<>]+>', ' ', msg).strip()
+			msg = msg.decode('cp1251')
+	send_msg(type, jid, nick, msg)
+
+
 global execute
 
-execute = [(3, 'gcalc', gcalc, 2, L('Google Calculator. Author: Vit@liy'))]
+execute = [(3, 'gcalc', gcalc, 2, L('Google Calculator. Author: Vit@liy')),
+	(3, 'define', define, 2, L('Definition for a word or phrase'))]
