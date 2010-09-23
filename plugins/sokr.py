5a06e2a6a09cd89a1388eeb0a5bc1f58a8db477c
poluphanov.vitaliy <poluphanov.vitaliy@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2010-09-23 20:12:43 +0000

    add: sokr.py - abbreviations

---
+++ plugins/sokr.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) Vit@liy
+
+def sokr(type, jid, nick, text):
+	if not text.strip(): msg = L('What?')
+	else:
+		target = '1-5'
+		if re.search('\A\d+?(-\d+?)? ', text): target, text = text.split(' ', 1)
+		query=urllib.urlencode({'q':text.encode('utf-8')})
+		sokr=httplib.HTTPConnection("www.sokr.ru")
+		sokr.request("GET","/search/?abbr=%s&abbr_exact=1" % query)
+		search=sokr.getresponse()
+		data=search.read()
+		results = re.findall('em class="got_clear">.+?</em></a>.+?<p class="value">(.+?)</p>' , data, re.S)
+		try: n1 = n2 = int(target)
+		except: n1, n2 = map(int, target.split('-'))
+		if not results: msg = L('I don\'t know!')
+		else:
+			if 0 < n1 <= n2 <= len(results): 
+				msg = L('Total found %s matches. Result(s) %s:\n') % (len(results), target)	
+				count = n1-1
+				for k in xrange(n1-1, n2):
+					count += 1
+					msg += '%s) %s\n' % (count, results[k].decode('utf8'))
+				msg = msg.replace('<br>', '')
+			else: msg = L('I don\'t know!')
+	send_msg(type, jid, nick, msg)
+
+global execute
+
+execute = [(3, 'sokr', sokr, 2, L('Abbreviations.\nExamples: sokr abbr, sokr 6 abbr, sokr 3-7 abbr'))]
