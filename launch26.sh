c3c03b3d2c17ee672e630b4cd14dd817deb63920
a.gorelow <a.gorelow@b2d7f3ce-29db-11de-a435-ffa8d773f76a>
2010-10-28 14:27:51 +0000

    fix: launch scripts for plugins list backup

--- launch26.sh
+++ launch26.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -e plugins/list.txt ] ; then rm -f plugins/list.txt.back && mv plugins/list.txt.back plugins/list.txt ; fi
+if [ -e plugins/list.txt ] ; then rm -f plugins/list.txt.back && mv plugins/list.txt plugins/list.txt.back ; fi
 svn up
 if [ ! -e plugins/list.txt ] ; then mv plugins/list.txt.back plugins/list.txt ; fi
-python2.7 isida.pypython2.6 isida.py
+python2.6 isida.py
